132624b5bd3a67259de7f75f3b1a0e06e79ce183
ashishgupta-97065 <ashishgupta97065@gmail.com>
2024-08-03 13:24:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,11 @@
+import streamlit as st
+
 if st.button("Clear All"):
     # Clears all singleton caches:
     st.experimental_singleton.clear()
 
 
-import streamlit as st
-# import pyautogui
- 
-# if st.button("Reset"):
-#     pyautogui.hotkey("ctrl","F5")
 
-import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
