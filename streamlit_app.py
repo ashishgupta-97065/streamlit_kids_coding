1c0e12e7b66adea31fe11b12dd66c2cecb6a9cb9
ashishgupta-97065 <ashishgupta97065@gmail.com>
2024-08-03 13:35:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,10 @@
 import streamlit as st
 import numpy as np
+
 if st.button("Restart"):
-    # st.session_state.value = "Foo"
     st.rerun()
 
 X1 = np.random.randint(low=0, high=10)
-
-
-
 
 st.title("🎈 My new app")
 st.write(X1)
