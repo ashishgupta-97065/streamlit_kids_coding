702d64d8f08dd9862a957b0bf7f4c483d353e4c1
ashishgupta-97065 <ashishgupta97065@gmail.com>
2024-08-03 13:31:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from random import randrange
-print(randrange(10))
+import numpy as np
+X1 = np.random.randint(low=0, high=10, size=(15,))
 
 st.title("🎈 My new app")
 st.write(
