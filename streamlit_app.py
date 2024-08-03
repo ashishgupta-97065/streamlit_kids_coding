1a1b313cba7da8b09256dbc1d2fff1a6887cd874
ashishgupta-97065 <ashishgupta97065@gmail.com>
2024-08-03 13:32:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import numpy as np
 X1 = np.random.randint(low=0, high=10, size=(15,))
 
 st.title("🎈 My new app")
+st.write(X1)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
