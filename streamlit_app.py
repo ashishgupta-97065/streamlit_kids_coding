29596f0e4865378dcb9f8378d7beb69d712e1434
ashishgupta-97065 <ashishgupta97065@gmail.com>
2024-08-04 13:08:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,9 @@ if 'count_value' not in st.session_state:
 X2 = st.number_input("YOUR ANSWER:", step=1)
 
 if st.button("check answer"):
-    st.write("Your answer", X2)
+    st.write("Actual answer: ", session_state.count_value)
+    st.write("Your answer: ", X2)
+    
     if X2==session_state.count_value:
         st.write("your are correct")
     else:
