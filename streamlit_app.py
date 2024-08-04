290a42dd3128a15c110781b4a3f4f9a2ced7eff8
ashishgupta-97065 <ashishgupta97065@gmail.com>
2024-08-04 12:24:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,21 +1,34 @@
 import streamlit as st
 import numpy as np
-# import eval
-
-# a = st.number_input("a")
-# b = st.number_input("b")
-# operator = st.text_input("operator")
-# # expression = a + operator + b # simple string concatenation
-# # result = eval(expression)
-# # st.write(result)
 
 if st.button("Restart"):
     st.rerun()
 
+
 X1 = np.random.randint(low=0, high=10)
 
-st.title("🎈 My new app")
 st.write(X1)
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+if st.button("PRINT"):
+    st.write(X1)
+
+
+
+# # import eval
+
+# # a = st.number_input("a")
+# # b = st.number_input("b")
+# # operator = st.text_input("operator")
+# # # expression = a + operator + b # simple string concatenation
+# # # result = eval(expression)
+# # # st.write(result)
+
+
+
+# X1 = np.random.randint(low=0, high=10)
+
+# st.title("🎈 My new app")
+# st.write(X1)
+# st.write(
+#     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+# )
