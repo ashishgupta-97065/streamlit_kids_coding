b8e30e243c895c6f69acb8fb252d713adbbb948a
ashishgupta-97065 <ashishgupta97065@gmail.com>
2024-08-04 13:14:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,16 +11,17 @@ X1 = np.random.randint(low=0, high=10)
 st.write(X1)
 
 # Initialize session state variables
-if 'count_value' not in st.session_state:
-    st.session_state.count_value = X1
+if 'answer' not in st.session_state:
+    st.session_state.answer = X1
 
 X2 = st.number_input("YOUR ANSWER:", step=1)
 
 if st.button("check answer"):
-    st.write("Actual answer: ", st.session_state.count_value)
+    a = st.session_state.answer
+    st.write("Actual answer: ", a)
     st.write("Your answer: ", X2)
     
-    if X2==st.session_state.count_value:
+    if X2==a:
         st.write("your are correct")
     else:
         st.write("your are incorrect")
